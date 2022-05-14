238a9b0ebefa568f977ef41c5b7e162a489a57fd
xiaoqi.xie <xiaoqi.xie@feiniu.com>
2022-05-14 12:37:37 +0800

    IC-2022.1 <xiaoqi.xie@SH-CODE-PC0101 Create baseRefactoring.xml, git.xml, ide-features-trainer.xml, project.default.xml, rainbow_brackets.xml, diff.xml, equalsHashCodeTemplates.xml, Enum.java, Interface.java, shared-indexes.xml, Default.xml, visualizationTool.xml, find.xml, ui.lnf.xml, DontShowAgainFeedbackService.xml, filetypes.xml, editor-font.xml, colors.scheme.xml, KeyPromoterXSettings.xml, maven.wrapper.mapping.xml, AnnotationType.java, FeatureSuggester.xml, Record.java, laf.xml, Class.java, jdk.table.xml, settingsSyncPlugins.xml, proxy.settings.xml, dbnavigator.xml, vcs.xml, codota.xml, toStringTemplates.xml, mybatis.xml, smart.tomcat.xml, editor.xml, usageView.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @Author ${USER}
+ * @Description: TODO 
+ * @Date ${DATE} 
+ * @Version V1.0
+**/
+public class ${NAME} {
+}
